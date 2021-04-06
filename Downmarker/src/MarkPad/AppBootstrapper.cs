b7f50b16f41c1f7caf512369aeb6bcb7985dc0ef
Thx_Lia <jonathan.lia@outlook.com>
2021-04-06 15:37:05 +0800

    Added base app framework - Autofac

--- Downmarker/src/MarkPad/AppBootstrapper.cs
+++ Downmarker/src/MarkPad/AppBootstrapper.cs
@@ -1,10 +1,100 @@
-﻿using Caliburn.Micro;
+﻿using System.Linq;
+using System.Windows;
+using Autofac;
+using Caliburn.Micro;
 using MarkPad.Shell;
+using MarkPad.Framework;
+using System;
+using System.Collections.Generic;
 
 namespace MarkPad
 {
     public class AppBootstrapper : Bootstrapper<ShellViewModel>
     {
+        private IContainer _Container;
 
+        protected override void Configure()
+        {
+            var builder = new ContainerBuilder();
+
+            SetupCaliburnMicroDefaults(builder);
+
+            builder.RegisterModule<EventAggregationAutoSubscriptionModule>();
+
+            _Container = builder.Build();
+        }
+
+        protected override void PrepareApplication()
+        {
+            Application.Startup += OnStartup;
+
+#if (!DEBUG)
+            Application.DispatcherUnhandledException += OnUnhandledException;
+#endif
+
+            Application.Exit += OnExit;
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            base.OnStartup(sender, e);
+        }
+
+        #region Autofac Stuff
+
+        private static void SetupCaliburnMicroDefaults(ContainerBuilder builder)
+        {
+            // register view models
+            builder.RegisterAssemblyTypes(AssemblySource.Instance.ToArray())
+                // must be a type with a name that ends with ViewModel
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                // registered as self
+                .AsSelf()
+                // always create a new one
+                .InstancePerDependency();
+
+            //Register views
+            builder.RegisterAssemblyTypes(AssemblySource.Instance.ToArray())
+                // must be a type with a name that ends with View
+                .Where(type => type.Name.EndsWith("View"))
+                // registered as self
+                .AsSelf()
+                // always create a new one
+                .InstancePerDependency();
+
+            // register the single window manager for this container
+            builder.Register<IWindowManager>(c => new WindowManager()).InstancePerLifetimeScope();
+            // register the single event aggregator for this container
+            builder.Register<IEventAggregator>(c => new EventAggregator()).InstancePerLifetimeScope();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            object instance;
+            if (string.IsNullOrEmpty(key))
+            {
+                if (_Container.TryResolve(service, out instance))
+                    return instance;
+            }
+            else
+            {
+                if (_Container.TryResolveNamed(key, service, out instance))
+                    return instance;
+            }
+
+            throw new Exception($"Could not locate any instances of contract {key ?? service.Name}.");
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _Container.Resolve(typeof(IEnumerable<>).MakeGenericType(service)) as IEnumerable<object>;
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _Container.InjectProperties(instance);
+        }
+
+        #endregion
     }
 }
---
+++ Downmarker/src/MarkPad/Framework/EventAggregationAutoSubscriptionModule.cs
@@ -0,0 +1,23 @@
+﻿using Autofac;
+using Autofac.Core;
+using Caliburn.Micro;
+
+namespace MarkPad.Framework
+{
+    public class EventAggregationAutoSubscriptionModule : Module
+    {
+        protected override void AttachToComponentRegistration(IComponentRegistry componentRegistry, IComponentRegistration registration)
+        {
+            registration.Activated += OnComponentActivated;
+        }
+
+        private void OnComponentActivated(object sender, ActivatedEventArgs<object> e)
+        {
+            // we never want to fail, so check for null (should never happen), and return if it is
+            if (e == null) return;
+
+            if (e.Instance is IHandle handler)
+                e.Context.Resolve<IEventAggregator>().Subscribe(handler);
+        }
+    }
+}
