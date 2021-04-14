ef174cfaf2e9408600efb4c85b2b1e9a9147fd4e
Thx_Lia <jonathan.lia@outlook.com>
2021-04-14 09:25:18 +0800

    Screen to gif （仅包含WPF部分）界面初始化

---
+++ ScreenToGif/ScreenToGif/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace ScreenToGif
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DragDown(object sender, MouseButtonEventArgs e) => DragMove();
+
+        private void BtnExit_Click(object sender, RoutedEventArgs e) => Close();
+    }
+}
